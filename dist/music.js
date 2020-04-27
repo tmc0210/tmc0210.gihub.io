5ee06a77a2c2ddbcc9c05e2ab02b8795685ef03a
tmc0210 <494521535@qq.com>
2020-04-27 21:09:46 +0800

    Site updated: 2020-04-27 21:09:46

---
+++ dist/music.js
@@ -0,0 +1,21 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    mini: true,
+    fixed: true,
+    autoplay: false,
+    theme: '#FADFA3',
+    loop: 'all',
+    order: 'random',
+    preload: 'auto',
+    volume: 0.7,
+    mutex: true,
+    listFolded: false,
+    listMaxHeight: 90,
+    lrcType: 3,
+    audio: [{
+        name: 'Momentum Mori',
+        artist: 'Danny Baranowsky',
+        url: 'https://music.163.com/song/media/outer/url?id=33316344.mp3',
+        cover: 'cover.jpg'
+    }]
+});
